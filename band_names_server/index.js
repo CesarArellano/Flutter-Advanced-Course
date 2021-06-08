636cd3fab87d6770f7d5010bc6c954cf48c509f2
CesarArellano <raywayday@gmail.com>
2021-06-08 14:24:53 -0500

    Socket.io Initial config

--- band_names_server/index.js
+++ band_names_server/index.js
@@ -2,13 +2,29 @@ const express = require('express');
 const path = require('path');
 require('dotenv').config();
 
+// App de Express
 const app = express();
+
+// Node server
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+// Socket Messages
+io.on('connection', client => {
+  console.log('Client Connected');
+  client.on('event', data => { /* … */ });
+  client.on('disconnect', () => {
+    console.log('Client Disconnected');
+  });
+});
+
+
 
 // Public path
 const publicPath = path.resolve( __dirname, 'public' );
 app.use(express.static( publicPath ));
 
-app.listen(process.env.PORT, (err) => {
+server.listen(process.env.PORT, (err) => {
   if( err ) throw new Error(err);
 
   console.log(`Servidor corriendo en puerto`, process.env.PORT);
