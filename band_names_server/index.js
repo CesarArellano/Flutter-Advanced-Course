f0405904239a3c4dd42ac552569dfd2c1ac23fa4
CesarArellano <raywayday@gmail.com>
2021-06-08 13:55:49 -0500

    Creating public path

--- band_names_server/index.js
+++ band_names_server/index.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
+
+// Public path
+const publicPath = path.resolve( __dirname, 'public' );
+app.use(express.static( publicPath ));
 
 app.listen(3000, (err) => {
   if( err ) throw new Error(err);
