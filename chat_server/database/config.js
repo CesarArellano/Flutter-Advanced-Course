347ae61e8112596f7cc11f89717b5cb869082889
cesar.arellano <cesar.arellano@gtec.com.mx>
2021-06-18 14:49:35 -0500

    Connecting MongoDB with Mongoose

--- chat_server/database/config.js
+++ chat_server/database/config.js
@@ -2,7 +2,13 @@ const mongoose = require('mongoose');
 
 const dbConnection = async () => {
   try {
-    console.log('init dbConfig');
+    await mongoose.connect( process.env.DB_CNN, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    });
+
+    console.log('DB Online');
   } catch(error) {
     console.log(error);
     throw new Error('Error en la base de datos - Hable con el admin xd');
