1fc668f401c51158190c89a2b31711779ae5185d
cesar.arellano <cesar.arellano@gtec.com.mx>
2021-06-19 13:34:13 -0500

    Knowing Express Validatore

--- chat_server/controllers/auth.js
+++ chat_server/controllers/auth.js
@@ -1,6 +1,17 @@
 const { response } = require('express');
+const { validationResult } = require('express-validator');
 
 const createUser = (req, res = response) => {
+  console.log(req);
+  const errors = validationResult(req);
+
+  if( !errors.isEmpty() ) {
+    return res.status(400).json({
+      ok: false,
+      errors: errors.mapped()
+    });
+  }
+
   res.json({
     ok: true,
     msg: 'Crear usuario'
--- chat_server/routes/auth.js
+++ chat_server/routes/auth.js
@@ -2,9 +2,13 @@
   path: api/login
 */
 const { Router } = require('express');
+const { check } = require('express-validator');
+
 const { createUser } = require('../controllers/auth');
 
 const router = Router();
-router.post('/new', createUser);
+router.post('/new',[
+  check('name', 'Name is required').not().isEmpty(),
+], createUser);
 
 module.exports = router;
