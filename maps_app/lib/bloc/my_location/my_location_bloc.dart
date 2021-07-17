4b5b69ebec30a6cbe406707b58206247417a7a7b
cesar.arellano <cesar.arellano@gtec.com.mx>
2021-07-16 20:02:08 -0500

    Implementing tracking in our app

--- maps_app/lib/bloc/my_location/my_location_bloc.dart
+++ maps_app/lib/bloc/my_location/my_location_bloc.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:geolocator/geolocator.dart' as Geolocator;
 import 'package:meta/meta.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -10,6 +11,21 @@ part 'my_location_state.dart';
 
 class MyLocationBloc extends Bloc<MyLocationEvent, MyLocationState> {
   MyLocationBloc() : super(MyLocationState());
+  
+  StreamSubscription<Geolocator.Position>? _positionSubscription;
+
+  void startTracking() {
+    this._positionSubscription = Geolocator.Geolocator.getPositionStream(
+      desiredAccuracy: Geolocator.LocationAccuracy.high,
+      distanceFilter: 10,
+    ).listen( ( Geolocator.Position position ) {
+      print(position);
+    });
+  }
+
+  void disposeTracking() {
+    this._positionSubscription?.cancel();
+  }
 
   @override
   Stream<MyLocationState> mapEventToState( MyLocationEvent event ) async* {
--- maps_app/lib/pages/map_page.dart
+++ maps_app/lib/pages/map_page.dart
@@ -1,8 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:maps_app/bloc/my_location/my_location_bloc.dart';
 
 
-class MapPage extends StatelessWidget {
+class MapPage extends StatefulWidget {
 
+  @override
+  _MapPageState createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  
+  @override
+  void initState() {
+    BlocProvider.of<MyLocationBloc>(context).startTracking();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    BlocProvider.of<MyLocationBloc>(context).disposeTracking();
+    super.dispose();
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
